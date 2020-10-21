e28f7d999e90450ae49a48aff74ac45e5889e025
sebasttianvelez <sebasttianvelez@gmail.com>
2020-10-21 14:13:59 -0500

    stable test

--- challenge-one/__tests__/async.test.js
+++ challenge-one/__tests__/async.test.js
@@ -1,8 +1,17 @@
 import { getData } from '../src/utils/promise'
 const API = 'https://dragon-ball-api.herokuapp.com/api/character/goku'
 
-// Para usar as
 it('debería retornar la información de goku', async () => {
   const response = await getData(API)
   expect(response.data.name).toBe('goku')
+})
+
+it('Debería fallar con url inválida', async () => {
+  expect.assertions(1)
+  // Estaríamos esperando un error
+  try {
+    await getData('asdas')
+  } catch (error) {
+    expect(error).not.toBeNull()
+  }
 })
